9ab6e92c55d8489b887efd77003f9761eb4f2743
ninakvach <ninikvachantiradze@icloud.com>
2020-09-23 08:58:05 +0300

    lesson2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-angular-app-J';
+  title = 'first-angular-app';
 
 }
